30951734b36b886b02722c75632163b161a39984
theallisonbrett <theallisonbrett@gmail.com>
2017-07-01 20:24:13 -0700

    Add Flickr search api

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
-
-function getInstagram(query) {
-console.log(query);
+function getFlickr(query) {
+    $.ajax({
+        type: "GET",
+        url: "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=31f89d39773fe830deb3f6af7199a266&tags=ruby&per_page=6&format=json&nojsoncallback=1&api_sig=10ae12d5428c7e8d9cf953baa4cc2c21",
+        dataType: "json",
+        success: function(response) {
+          console.log("success!");
+        },
+        error: function(xhr, status, e) {
+            console.log(status, e);
+        }
+    });
 }
 
 
@@ -10,7 +19,7 @@ function formSubmit() {
     $("form").submit(function(e) {
         e.preventDefault();
         var query = $("#searchterm").val();
-        getInstagram(query);
+        getFlickr(query);
 
     });
 }
