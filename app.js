814993038e1b661d6c2e1c9048b85f7d81278ee5
theallisonbrett <theallisonbrett@gmail.com>
2017-07-02 17:13:08 -0700

    Replace kitten tag with query variable

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
-var proxy = 'https://cors-anywhere.herokuapp.com/';
-var url = 'https://api.flickr.com/services/feeds/photos_public.gne?tags=kitten&format=json';
+
 
 function getFlickr(query) {
+  var proxy = 'https://cors-anywhere.herokuapp.com/';
+  var url = 'https://api.flickr.com/services/feeds/photos_public.gne?tags='+query+'&format=json';
     $.ajax({
         type: "GET",
         url: proxy + url,
         // dataType: "json",
         success: function(response) {
-        console.log(response)
+        console.log(response);
 
         },
         error: function(xhr, status, e) {
