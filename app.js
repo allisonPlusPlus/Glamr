90b7412de608f13ca8ec6c9b68a96bb32dc8edb9
theallisonbrett <theallisonbrett@gmail.com>
2017-07-02 17:08:24 -0700

    Return JSON successfully

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
+var proxy = 'https://cors-anywhere.herokuapp.com/';
+var url = 'https://api.flickr.com/services/feeds/photos_public.gne?tags=kitten&format=json';
+
 function getFlickr(query) {
     $.ajax({
         type: "GET",
-        url: "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=31f89d39773fe830deb3f6af7199a266&tags=ruby&per_page=6&format=json&nojsoncallback=1&api_sig=10ae12d5428c7e8d9cf953baa4cc2c21",
-        dataType: "json",
+        url: proxy + url,
+        // dataType: "json",
         success: function(response) {
-          console.log("success!");
+        console.log(response)
+
         },
         error: function(xhr, status, e) {
             console.log(status, e);
