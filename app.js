6b7d9f8eb0208a1e0230d18a36ad73eb21ba9a1a
theallisonbrett <theallisonbrett@gmail.com>
2017-07-27 19:02:18 -0700

    Successfully display youtube results

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ function displayYoutubeResults(response) {
     var watchUrl = "https://www.youtube.com/watch?v=";
 
           //Display thumbnail
-          $(`result${i+1}`).find("img").attr('src', item.snippet.thumbnails.medium.url);
+          $(`#result${i+1}`).find("img").attr('src', item.snippet.thumbnails.medium.url);
 
           // //Make thumbnail link to video
           $(`#result${i+1}`).find('a').attr('href', watchUrl + item.id.videoId);
@@ -45,13 +45,22 @@ response.items.forEach(appendResults);
 
 }
 
+function clearImages(query) {
+  for (var i = 0; i < 6; i++) {
+      //Render thumbnail images
+      $(`#result${i+1}`).find("img").attr('src', "");
+      //Render thumbnail links
+      $(`#result${i+1}`).find("a").attr('href', "");
+    }
+ getYoutube(query)
+
+}
 
 
 function chooseVideo(query) {
 console.log("chooseVideo", query)
   $(".videos").on("click", function() {
-     $(".js-search-results").empty();
-     getYoutube(query)
+     clearImages(query);
   });
 
 }
